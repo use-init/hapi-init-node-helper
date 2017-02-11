89e9838236448fb0145b5aef7b08c6f0f49e8963
Hans Christian Reinl <info@drublic.de>
2017-02-11 08:39:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Export all helpers
+ * @type {Object}
+ */
+module.exports = {
+  hashString: require('./lib/hashString'),
+  isFile: require('./lib/isFile'),
+  readJsonFile: require('./lib/readJsonFile'),
+  removeFile: require('./lib/removeFile'),
+  writeFile: require('./lib/writeFile')
+};
